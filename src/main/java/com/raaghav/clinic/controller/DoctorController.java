87c94b02fa71d9fe4ca66b5735af0899794f0ce2
Raaghav-m <raaghavmanivel2005@gmail.com>
2026-08-02 12:17:37 +0530

    Doctor Module Complete:With API Functionality

--- src/main/java/com/raaghav/clinic/controller/DoctorController.java
+++ src/main/java/com/raaghav/clinic/controller/DoctorController.java
@@ -4,7 +4,10 @@ import com.raaghav.clinic.dto.DoctorRequestDTO;
 import com.raaghav.clinic.dto.DoctorResponseDTO;
 import com.raaghav.clinic.service.DoctorService;
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/doctors")
@@ -18,4 +21,23 @@ public class DoctorController {
     public DoctorResponseDTO addDoctor(@Valid @RequestBody DoctorRequestDTO requestDTO){
         return service.addDoctor(requestDTO);
     }
+
+    @GetMapping
+    public List<DoctorResponseDTO> getAllDoctors(){
+        return service.getAllDoctors();
+    }
+    @GetMapping("/{id}")
+    public DoctorResponseDTO getDoctorById(@PathVariable Long id){
+        return service.getDoctorById(id);
+    }
+    @PutMapping("/{id}")
+    public DoctorResponseDTO updateDoctor(@PathVariable Long id,@Valid @RequestBody DoctorRequestDTO requestDTO){
+        return service.updateDoctor(id,requestDTO);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable Long id){
+        service.deleteDoctor(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/raaghav/clinic/service/DoctorService.java
+++ src/main/java/com/raaghav/clinic/service/DoctorService.java
@@ -2,12 +2,13 @@ package com.raaghav.clinic.service;
 
 import com.raaghav.clinic.dto.DoctorRequestDTO;
 import com.raaghav.clinic.dto.DoctorResponseDTO;
-import com.raaghav.clinic.dto.PatientResponseDTO;
 import com.raaghav.clinic.entity.Doctor;
+import com.raaghav.clinic.exception.ResourceNotFoundException;
 import com.raaghav.clinic.mapper.DoctorMapper;
 import com.raaghav.clinic.repository.DoctorRepository;
-import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DoctorService {
@@ -19,5 +20,29 @@ public class DoctorService {
         Doctor newDoctor= DoctorMapper.toEntity(doctorRequestDTO);
         Doctor savedDoctor=doctorRepository.save(newDoctor);
         return DoctorMapper.toResponse(savedDoctor);
+    }
+    public List<DoctorResponseDTO> getAllDoctors(){
+        return doctorRepository.findAll().stream().map(DoctorMapper::toResponse).toList();
+    }
+    public DoctorResponseDTO getDoctorById(Long id){
+        Doctor doctor= doctorRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("The doctor is not found with the given id"));
+        return DoctorMapper.toResponse(doctor);
+    }
+
+    public DoctorResponseDTO updateDoctor(Long id, DoctorRequestDTO doctorRequestDTO){
+        Doctor doctor=doctorRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("the doctor is not found with the given id"));
+        doctor.setStartTime(doctorRequestDTO.getStartTime());
+        doctor.setSpecialization(doctorRequestDTO.getSpecialization());
+        doctor.setPhone(doctorRequestDTO.getPhone());
+        doctor.setName(doctorRequestDTO.getName());
+        doctor.setExperience(doctorRequestDTO.getExperience());
+        doctor.setEndTime(doctorRequestDTO.getEndTime());
+        doctor.setConsultationFee(doctorRequestDTO.getConsultationFee());
+        doctorRepository.save(doctor);
+        return DoctorMapper.toResponse(doctor);
+    }
+    public void deleteDoctor(Long id){
+        Doctor doctor=doctorRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("the doctor is not found with the given id"));
+        doctorRepository.delete(doctor);
     }
 }
